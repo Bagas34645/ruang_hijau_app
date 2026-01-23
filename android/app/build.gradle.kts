572dfff0d741c2028b93990c61b0c60feea7ce53
bagas34645 <bagasabiyu1234@gmail.com>
2026-01-23 14:09:09 +0700

    Add release signing configuration and keystore for app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,6 +19,15 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "ruang_hijau_key"
+            keyPassword = "ruanghijau"
+            storeFile = file("ruang_hijau_release.jks")
+            storePassword = "ruanghijau"
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.ruang_hijau_app"
@@ -32,9 +41,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
